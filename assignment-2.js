a318f57e314c26f3daaebe8cd34acdb63ba7f629
rfpete <rfpete@gmail.com>
2019-02-01 12:00:30 -0800

    finished assignments 1-3

---
+++ assignment-2.js
@@ -0,0 +1,9 @@
+let words = ['A', 'mouse', 'took', 'a', 'stroll', 'through', 'the', 'deep', 'dark', 'wood.'];
+
+console.log(words);
+
+let sentence = words.join(' ');
+
+console.log(sentence);
+
+console.log(words[2]);
---
+++ assignment-3.js
@@ -0,0 +1,5 @@
+let words = ['A', 'mouse', 'took', 'a', 'stroll', 'through', 'the', 'deep', 'dark', 'wood.'];
+
+for (let i = 0; i < words.length; i++) {
+  console.log(i + ', ' + words[i]);
+}
